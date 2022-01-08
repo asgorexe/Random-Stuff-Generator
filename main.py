0cbe26269697ffd49365878321bdd615f67c8cea
Relic374 <75509736+asgorexe@users.noreply.github.com>
2022-01-08 12:10:39 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+import string
+
+
+for x in range(21):
+
+    characters = string.printable
+
+    random_character = random.choice(characters)
+
+    random_bits = random.getrandbits(5)
+    random_int = random.randrange(0, 12, 1)
+
+    str_int = str(random_int)
+    str_bits = str(random_bits)
+
+    print("-----------------")
+    print("Random Int: " + str_int)
+
+    print("Extras:")
+
+    print("Random Range: " + random_character)
+    print("Random Bits: " + str_bits)
