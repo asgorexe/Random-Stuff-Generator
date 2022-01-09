5525be7737c344a8ea44a2fa7f1efde55b3b0873
Relic374 <75509736+asgorexe@users.noreply.github.com>
2022-01-08 19:03:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+# Made by Relic374
+# Version 0.3
+
 import random
 import string
 
 
-for x in range(21):
+cycle_amount = {}
 
+print("How many times should the random number be given?:")
+cycle_amount = input()
+
+cycle_amount = int(cycle_amount)
+
+
+for x in range(cycle_amount):
+    
+    cycle_amount = {}
+    
     characters = string.letters + string.digits + string.punctuation
 
     random_character = random.choice(characters)
+    
+    print("How many times should the random number be given?:")
+    cycle_amount = input()
 
+    cycle_amount = int(cycle_amount)
+    
     random_bits = random.getrandbits(5)
     random_int = random.randrange(0, 12, 1)
 
